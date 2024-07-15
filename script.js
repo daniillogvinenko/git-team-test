70f4fb81e7d0f0bf9525c28cc31d79a65dafa62e
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-07-15 13:11:05 +0300

    reset

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-console.log("hello from JS");
-
-console.log("second console log");
-
-if (Math.random() < 0.5) {
-    console.log("random number is less than 0.5");
-}
-
-console.log("4");
-
-console.log("4");
-
-console.log("feature 4 fix");
+console.log("hello");
--- script2.js
+++
@@ -1,5 +0,0 @@
-console.log("hello from script2.js");
-
-console.log("2nd");
-
-console.log("3rd");
